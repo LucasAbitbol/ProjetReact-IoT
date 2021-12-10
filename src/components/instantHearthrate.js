19c4ca83c4c2dad66a2fce5e76693330134a519b
LucasAbitbol <lucasabitbol6@gmail.com>
2021-12-10 10:36:33 +0100

    Génération aléatoire entre 40 et 200 bpm à partir d'une variable

---
+++ src/components/instantHearthrate.js
@@ -0,0 +1,8 @@
+function InstantHearthrate() {
+    const bpm = Math.floor(Math.random()*160) + 40;
+    return (
+        "Rythme cardiaque : " + bpm
+    )
+  }
+
+  export default InstantHearthrate;
