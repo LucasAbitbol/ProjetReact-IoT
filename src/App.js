1fe88295799bb254775f7cd3daf2fa54a911154e
LucasAbitbol <lucasabitbol6@gmail.com>
2021-12-10 15:06:53 +0100

    Ajout du graphe

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import InstantHearthrate from './components/instantHearthrate';
 import { useEffect, useState } from 'react';
+import Graph from './components/graph';
 
 function randomBpm() {
   return Math.floor(Math.random() * 160) + 40;
@@ -9,25 +10,37 @@ function randomBpm() {
 
 function App() {
   const [bpms, setBpms] = useState([50]);
+  
+  const d = new Date();
+let h = d.getHours();
+let m = d.getMinutes();
+let s = d.getSeconds();
+let time = h + "h" + m + "m" + s + "s";
 
   useEffect(() => {
     setInterval(() => {
-      setBpms((prev) => {
-        return [...prev, randomBpm()]
-    });
-   }, 5000);
-  }, []);
+      setBpms((prev) => {      
+        const bpm = {
+        timestamp: time,
+        hearthrate: randomBpm(),
+      }
+      return [...prev, bpm]
+      });
+    }, 5000)
+}, [])
 
   const bpm = bpms[bpms.length - 1 ];
 
   console.log(bpms);
+  console.log(bpm.timestamp);
 
   return (
     <div className="App">
       <header className="App-header">
         <p>Monitoring de rythme cardiaque</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <InstantHearthrate data={bpm} />
+        <InstantHearthrate data={bpm.hearthrate} />
+        <Graph data={bpms} />
       </header>
     </div>
   );
--- src/components/instantHearthrate.js
+++ src/components/instantHearthrate.js
@@ -2,6 +2,8 @@ function InstantHearthrate(props) {
   const { data } = props;
   let classBpm = 'bpmNormal';
 
+  if (!data) {return (<p>Pas de valeur</p>)}
+
   if (data > 40 && data <= 70) {
     classBpm = 'bpmPasNormal';
   }
@@ -9,7 +11,7 @@ function InstantHearthrate(props) {
     classBpm = 'bpmPasNormal';
   }
 
-  return <p class={classBpm}> Rythme cardiaque : {data}</p>;
+  return <p className={classBpm}> Rythme cardiaque : {data}</p>;
 }
 
 export default InstantHearthrate;
